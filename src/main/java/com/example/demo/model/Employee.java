d3a52a207ece0fddc5a3998476b3b03562e760b3
rezamahesaa <reza.mahesa.azandi@gmail.com>
2024-08-01 10:27:52 +0700

    Model department, employee, region

---
+++ src/main/java/com/example/demo/model/Employee.java
@@ -0,0 +1,112 @@
+package com.example.demo.model;
+
+import java.time.LocalDate;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+// import javax.persistence.OneToOne;
+import javax.persistence.Table;
+
+// import com.fasterxml.jackson.annotation.JsonIgnore;
+
+@Entity
+@Table(name = "tb_m_employee")
+public class Employee {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column
+    private Integer id;
+    
+    @Column
+    private String name;
+
+    @Column
+    private String email;
+
+    @Column
+    private LocalDate birthDate;
+
+    @Column
+    private String address;
+
+    @Column
+    private String phone;
+
+    // @OneToOne(mappedBy = "employee")
+    // @JsonIgnore
+    // private User user;
+
+
+    public Employee() {
+    }
+
+    public Employee(Integer id, String name, String email, LocalDate birthDate, String address, String phone) {
+        this.id = id;
+        this.name = name;
+        this.email = email;
+        this.birthDate = birthDate;
+        this.address = address;
+        this.phone = phone;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public LocalDate getBirthDate() {
+        return birthDate;
+    }
+
+    public void setBirthDate(LocalDate birthDate) {
+        this.birthDate = birthDate;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    // public User getUser() {
+    //     return user;
+    // }
+
+    // public void setUser(User user) {
+    //     this.user = user;
+    // }
+
+    
+}
