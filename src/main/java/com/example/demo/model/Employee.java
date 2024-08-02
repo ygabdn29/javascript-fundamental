b0ad54a8133d4873e8c4f22e0c40de030385fa82
GitHubFanstein <158123195+GitHubFanstein@users.noreply.github.com>
2024-08-02 09:29:28 +0700

    Update Employee.java

--- src/main/java/com/example/demo/model/Employee.java
+++ src/main/java/com/example/demo/model/Employee.java
@@ -1,58 +1,108 @@
-package com.example.demo5.model;
+package com.example.demo.model;
+
+import java.time.LocalDate;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.OneToOne;
 import javax.persistence.Table;
 
 @Entity
-@Table(name = "tb_m_user")
-public class User {
+@Table(name = "tb_m_employee")
+public class Employee {
   @Id
   @GeneratedValue(strategy = GenerationType.IDENTITY)
-  @Column(name = "id")
+  @Column
   private Integer id;
 
-  @Column(name = "password")
-  private String password;
+  @Column
+  private String name;
 
-  @Column(name = "username")
-  private String username;
+  @Column
+  private String email;
 
+  @Column
+  private LocalDate birthDate;
 
-  public User() {}
+  @Column
+  private String address;
 
-  public User(Integer id, String password, String username) {
+  @Column
+  private String phone;
+
+  // @OneToOne(mappedBy = "employee")
+  // // @JsonIgnore
+  // private User user;
+
+  public Employee() {
+  }
+
+  public Employee(Integer id, String name, String email, LocalDate birthDate, String address, String phone) {
     this.id = id;
-    this.password = password;
-    this.username = username;
-    }
-    
-    public Integer getId() {
-        return id;
-    }
+    this.name = name;
+    this.email = email;
+    this.birthDate = birthDate;
+    this.address = address;
+    this.phone = phone;
+  }
+
+  public Integer getId() {
+    return id;
+  }
 
   public void setId(Integer id) {
     this.id = id;
   }
 
-  public String getPassword() {
-    return password;
+  public String getName() {
+    return name;
   }
 
-  public void setPassword(String password) {
-    this.password = password;
+  public void setName(String name) {
+    this.name = name;
   }
 
-  public String getUsername() {
-    return username;
+  public String getEmail() {
+    return email;
   }
 
-  public void setUsername(String username) {
-    this.username = username;
+  public void setEmail(String email) {
+    this.email = email;
   }
 
-    
+  public LocalDate getBirthDate() {
+    return birthDate;
+  }
+
+  public void setBirthDate(LocalDate birthDate) {
+    this.birthDate = birthDate;
+  }
+
+  public String getAddress() {
+    return address;
+  }
+
+  public void setAddress(String address) {
+    this.address = address;
+  }
+
+  public String getPhone() {
+    return phone;
+  }
+
+  public void setPhone(String phone) {
+    this.phone = phone;
+  }
+
+  // public User getUser() {
+  // return user;
+  // }
+
+  // public void setUser(User user) {
+  // this.user = user;
+  // }
+
 }
