5cc0f346c7b3fd41a8fc1208e3f211ee19f8ce00
GitHubFanstein <158123195+GitHubFanstein@users.noreply.github.com>
2024-08-02 09:31:34 +0700

    Update UserServiceImpl.java

--- src/main/java/com/example/demo/service/implementation/UserServiceImpl.java
+++ src/main/java/com/example/demo/service/implementation/UserServiceImpl.java
@@ -1,48 +1,36 @@
-package com.example.demo.service.implementation;
+package com.example.demo5.service.implementation;
 
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import com.example.demo.model.User;
-import com.example.demo.repository.UserRepository;
-import com.example.demo.service.UserService;
+import com.example.demo5.model.User;
+import com.example.demo5.repository.UserRepository;
+import com.example.demo5.service.UserService;
 
 @Service
-public class UserServiceImpl implements UserService{
-      @Autowired
+public class UserServiceImpl implements UserService {
+    @Autowired
     private UserRepository userRepository;
 
-    @Override
-    public List<User> get() {
-       return userRepository.findAll();
-    }
 
-    @Override
-    public User get(Integer key) {
-        return userRepository.findById(key).orElse(null);
-    }
-
-    @Override
-    public Boolean save(User entity) {
-       User user = userRepository.save(entity);
-        return user.getId().equals(null);
-    }
-
-    @Override
-    public Boolean delete(Integer idInteger) {
-        userRepository.deleteById(idInteger);
-        return userRepository.findById(idInteger).isEmpty();
-    }
-
-
+    // validasi untuk cek username dan pasword 
     public User authenticate(String username, String password) {
         User user = userRepository.findByUsername(username);
         if (user != null && user.getPassword().equals(password)) {
-            return user;
+          return user;
         }
         return null;
-    }
+      }
 
+      @Override
+      public List<User> get() {
+        return userRepository.findAll();
+      }
+    
+    //   @Override
+    //   public User get(Integer key) {
+    //     return userRepository.findById(key).orElse(null);
+    //   }
 }
