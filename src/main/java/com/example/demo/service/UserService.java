48745698ad1a3afabc7c86a02e25589f557b85b9
GitHubFanstein <158123195+GitHubFanstein@users.noreply.github.com>
2024-08-02 09:31:59 +0700

    Update UserService.java

--- src/main/java/com/example/demo/service/UserService.java
+++ src/main/java/com/example/demo/service/UserService.java
@@ -4,5 +4,5 @@ import com.example.demo.model.User;
 import com.example.demo.service.generic.GenericService;
 
 public interface UserService extends GenericService<User, Integer> {
-    
+     public User authenticate(String username, String password);
 }
